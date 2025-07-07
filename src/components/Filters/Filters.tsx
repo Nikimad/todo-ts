4822a5f7ee1308206706093934ac0179018af193
Nikita Savelev <pybepod@gmail.com>
2025-07-07 18:49:00 +0300

    Add src/components/Filters

---
+++ src/components/Filters/Filters.tsx
@@ -0,0 +1,35 @@
+import type { FC } from "react";
+import type { FilterType, FilterValueType } from "../../models";
+
+import Filter from "../Filter";
+
+interface FiltersProps {
+  currentFilterValue: FilterValueType;
+  onSelect: (filterValue: FilterValueType) => void;
+}
+
+const Filters: FC<FiltersProps> = ({ currentFilterValue, onSelect }) => {
+  const filters: FilterType[] = [
+    {
+      title: "All",
+      value: "all",
+    },
+    { title: "Active", value: "active" },
+    { title: "Completed", value: "completed" },
+  ];
+
+  return (
+    <div>
+      {filters.map((filter) => (
+        <Filter
+          key={filter.value}
+          filter={filter}
+          currentFilterValue={currentFilterValue}
+          onSelect={onSelect}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Filters;
