24b651259417b0cde23b5f0737b3d1e1d026ba3a
Nikita Savelev <pybepod@gmail.com>
2025-07-08 13:23:33 +0300

    Refactor index.html, src/App.tsx, src/main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import List from "./components/List"
+import Main from "./components/Main";
+import List from "./components/List";
 
 const App = () => {
-  return <List />
-}
+  return (
+    <Main>
+      <h1 className="title">TODOS</h1>
+      <List />
+    </Main>
+  );
+};
 
-export default App
+export default App;
